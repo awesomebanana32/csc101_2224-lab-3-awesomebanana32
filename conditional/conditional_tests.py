4f9c5a641b01016ad2e8c8eafe8badafaa68eb9b
awesomebanana32 <102541860+awesomebanana32@users.noreply.github.com>
2022-04-15 14:02:56 -0700

    pushing code

--- conditional/conditional_tests.py
+++ conditional/conditional_tests.py
@@ -5,38 +5,49 @@ class TestCases(unittest.TestCase):
     def test_max_101(self):
         condi = conditional.max_101(1,6)
         print("this is the max", condi)
+        self.assertAlmostEqual(condi, 6)
 
     def test_max_101_again(self):
         condi = conditional.max_101(10,1)
         print("this is the max", condi)
+        self.assertAlmostEqual(condi, 10)
 
     def test_max_of_three(self):
         three = conditional.max_of_three(10, 1, 2)
         print("this is the max of three:", three)
+        self.assertAlmostEqual(three, 10)
 
     def test_max_of_three_again(self):
         three = conditional.max_of_three(0, 1, 20)
         print("this is the max of three:", three)
+        self.assertAlmostEqual(three, 20)
 
     def test_max_of_three_again_2(self):
         three = conditional.max_of_three(0, 100, 20)
         print("this is the max of three:", three)
+        self.assertAlmostEqual(three, 100)
 
     def test_rental_late_fee(self):
         fee = conditional.rental_late_fee(0)
         print("this is the late fee:", fee)
+        self.assertAlmostEqual(fee, 0)
+
 
     def test_rental_late_fee_1(self):
         fee = conditional.rental_late_fee(7)
         print("this is the late fee:", fee)
+        self.assertAlmostEqual(fee, 5)
 
     def test_rental_late_fee_2(self):
         fee = conditional.rental_late_fee(15)
         print("this is the late fee:", fee)
+        self.assertAlmostEqual(fee, 7)
 
     def test_rental_late_fee_3(self):
         fee = conditional.rental_late_fee(20)
         print("this is the late fee:", fee)
+        self.assertAlmostEqual(fee, 19)
+
 
     def test_rental_late_fee_4(self):
         fee = conditional.rental_late_fee(100)
--- logic/logic_tests.py
+++ logic/logic_tests.py
@@ -5,18 +5,22 @@ class TestCases(unittest.TestCase):
     def test_case(self):
         joe = logic.is_even(4)
         print("this is even", joe)
+        self.assertAlmostEqual(joe, True)
 
     def test_case_again(self):
         joe = logic.is_even(5)
         print("this is even", joe)
+        self.assertAlmostEqual(joe, False)
 
     def test_in_an_interval(self):
         range_num = logic.in_an_interval(3)
         print("this is in range", range_num)
+        self.assertAlmostEqual(range_num, True)
 
     def test_in_an_interval_again(self):
         range_num = logic.in_an_interval(1000000)
         print("this is in range", range_num)
+        self.assertAlmostEqual(range_num, False)
 
 # Run the unit tests.
 if __name__ == '__main__':
